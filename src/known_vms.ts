b54866dbbfd0038a101b418a608b83472fbd7ddd
binhnd <nguyenduybinh@gapo.com.vn>
2022-01-20 09:29:25 +0700

    fix vm name for blockchain tabs

--- src/known_vms.ts
+++ src/known_vms.ts
@@ -13,19 +13,19 @@ const dict: VMDict = {
         name: 'platformvm',
         fullName: 'Platform',
         documentation:
-            'https://github.com/ava-labs/avalanchego/tree/master/vms/platformvm',
+            'https://github.com',
     },
     jvYyfQTxGMJLuGWa55kdP2p2zSUYsQ5Raupu4TW34ZAUBAbtq: {
         name: 'avm',
-        fullName: 'Ava',
+        fullName: 'EZC',
         documentation:
-            'https://github.com/ava-labs/avalanchego/tree/master/vms/avm',
+            'https://github.com',
     },
     mgj786NP7uDwBCcq6YwThhaN8FLyybkCa4zBWTQbNgmK6k9A6: {
         name: 'evm',
         fullName: 'Ethereum',
         documentation:
-            'https://github.com/ava-labs/avalanchego/tree/master/vms/evm',
+            'https://github.com',
     },
 }
 

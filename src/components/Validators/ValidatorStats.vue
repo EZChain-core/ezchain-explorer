f2c691f2e5a9dfb8d009bfc1cf5400d582e82522
enixdark <cqshinn92@gmail.com>
2022-03-07 20:39:03 +0700

    feat (update): change reward estimate

--- src/components/Validators/ValidatorStats.vue
+++ src/components/Validators/ValidatorStats.vue
@@ -96,6 +96,71 @@ import { Mixins, Component } from 'vue-property-decorator'
 import TooltipMeta from '@/components/Home/TopInfo/TooltipMeta.vue'
 import { bigToDenomBig } from '@/helper'
 import { PlatformGettersMixin } from '@/store/modules/platform/platform.mixins'
+import Big from 'big.js'
+import BN from 'bn.js'
+
+const SECONDS_PER_YEAR = 31536000 //31556952
+
+const RAT_1 = new Big(1)
+const RAT_0 = new Big(1)
+const BIG_2 = new Big(2)
+const BASE_TS = new BN('1000000000000000000000000')
+
+const PC_BASE = new Big(50).div(100)
+const PC_STEP = new Big(-4).div(100)
+const REWARD_POOL = new Big(15).div(100)
+
+
+function convert(n: any): any {
+    const sign: string = +n < 0 ? '-' : ''
+    let toStr: string = n.toString()
+    if (!/e/i.test(toStr)) {
+        return n
+    }
+    toStr = toStr.replace(/^-/, '')
+    const data: Array<any> = toStr.replace(/^([0-9]+)(e.*)/, '$1.$2').split(/e|\./)
+    const [lead, decimal, pow]: Array<any> = data
+    if (+pow < 0) {
+        return sign + '0.' + '0'.repeat(Math.max(Math.abs(pow) - 1 || 0, 0)) + lead + decimal
+    } else {
+        let p: string
+        if (+pow >= decimal.length) {
+            p = decimal + '0'.repeat(Math.max(+pow - decimal.length || 0, 0))
+        } else {
+            p = decimal.slice(0, +pow) + '.' + decimal.slice(+pow)
+        }
+        return sign + lead + p
+    }
+}
+
+
+function rewardPercent(ts: number): Big {
+    let _b: BN
+    const x: Big = new Big(ts).div(new Big(BASE_TS.toString()))
+    const tsLen: number = parseInt(convert(ts), 10).toString(2).length
+    const baseLen: number = parseInt(convert(BASE_TS), 10).toString(2).length
+    const bitLength: number = tsLen - baseLen
+    const b: BN = new BN(bitLength)
+    if (b.cmp(new BN(0)) == -1) {
+        _b = new BN(-1)
+    } else {
+        _b = b
+    }
+    const a: BN = new BN(BIG_2.div(1).toString()).pow(_b)
+    let r: Big = x.div(a.toString())
+    r = r.add(b.toString())
+    r = r.sub(RAT_1)
+    let p: Big = r.mul(PC_STEP)
+    p = p.add(PC_BASE)
+    if(p.gt(PC_BASE)){
+        p = PC_BASE
+    } else if (p.s < 0) {
+        p = RAT_1
+    }
+    return p
+}
+
+
 
 @Component({
     components: {
@@ -126,7 +191,8 @@ export default class ValidatorStats extends Mixins(PlatformGettersMixin) {
     }
 
     get annualStakingRewardPercentage(): string {
-        const APR = this.$store.state.Platform.annualStakingRewardPercentage
+        const totalStake = parseInt(this.getTotalStake().toString())
+        const APR = rewardPercent(totalStake * 10**9).mul(100)
         return `${APR.toFixed(1)}%`
     }
 }
